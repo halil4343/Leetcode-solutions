41972f2c4f1f9a0269021ace18d833c24752e626
halil4343 <152891242+halil4343@users.noreply.github.com>
2024-01-09 14:45:39 +0300

    Delete 0009-palindrome-number directory

--- 0009-palindrome-number/0009-palindrome-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        st = str(x)
-        rev = st[::-1]
-        if rev==st:
-            return True
-        else:
-            return False
-        
